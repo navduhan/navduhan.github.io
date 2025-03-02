689bc5455b13efe135b826fe3b46a6ad85825034
navduhan <duhan27dec@gmail.com>
2025-03-02 13:32:35 -0600

    Updates

---
+++ _next/static/chunks/9c4e2130-e6006120454d0734.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[512],{46280:(t,e,n)=>{n.d(e,{_kA:()=>i});var r=n(93435);function i(t){return(0,r.k5)({tag:"svg",attr:{viewBox:"0 0 512 512"},child:[{tag:"path",attr:{fill:"none",strokeLinecap:"round",strokeLinejoin:"round",strokeWidth:"48",d:"m112 184 144 144 144-144"},child:[]}]})(t)}}}]);
